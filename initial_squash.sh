2b1a2d22d14053f24732e96410a09137f8f8fae2
Danielle Tomlinson <dan@tomlinson.io>
2017-04-15 14:08:31 +0200

    use git -C

--- initial_squash.sh
+++ initial_squash.sh
@@ -4,18 +4,9 @@ set -e
 set -o pipefail
 set -x
 
-USE_CIRCLECI_SPECS_MIRROR=1
-
 clone_repo() {
-  if [[ $USE_CIRCLECI_SPECS_MIRROR ]]; then
-    echo "> Downloading Repo Script from S3"
-    aws s3 --no-sign-request cp "s3://cocoapods-specs/latest.tar.gz" "latest.tar.gz"
-    tar -xzf latest.tar.gz
-    mv master Specs
-  else
-    echo "> Cloning CocoaPods/Specs"
-    git clone https://github.com/CocoaPods/Specs.git Specs
-  fi
+  echo "> Cloning CocoaPods/Specs"
+  git clone https://github.com/CocoaPods/Specs.git Specs
 }
 
 measure_repo() {
@@ -24,22 +15,32 @@ measure_repo() {
   du -sh Specs/.git
 }
 
+git_exec() {
+  git -C Specs $@
+}
+
 squash_branch() {
   BRANCH_NAME=$1
-  git checkout --orphan "new_$BRANCH_NAME"
-  git add .
-  git commit -m "Squashed!"
-  git branch -D "$BRANCH_NAME"
-  git checkout -b "$BRANCH_NAME"
-  git branch -D "new_$BRANCH_NAME"
-  git checkout master
+  pushd Specs
+  git_exec checkout --orphan "new_$BRANCH_NAME"
+  git_exec add .
+  git_exec commit -m "Squashed!"
+  git_exec branch -D "$BRANCH_NAME"
+  git_exec checkout -b "$BRANCH_NAME"
+  git_exec branch -D "new_$BRANCH_NAME"
+  git_exec checkout master
+  popd
 }
 
 tag_sharding_branch() {
-  git checkout "predates_sharding_branch"
-  git tag -d "v0.32.1"
-  git tag "v0.32.1"
-  git checkout master 
+  git_exec checkout "predates_sharding_branch"
+  git_exec tag -d "v0.32.1"
+  git_exec tag "v0.32.1"
+  git_exec checkout master 
+}
+
+clean_repo() {
+  git_exec gc --prune=now --aggressive
 }
 
 clone_repo
@@ -47,4 +48,6 @@ measure_repo
 squash_branch "master"
 squash_branch "predates_sharding_branch"
 tag_sharding_branch
+measure_repo
+clean_repo
 measure_repo
